af2276d7c40871bd944e1e10e3c845876fb311e0
oihugu <53376595+oihugu@users.noreply.github.com>
2020-11-21 20:24:23 -0300

    gwyddie changes, manual merge -- peripheral files

---
+++ game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "doublyLinkedList.h"
+
+#define MIN_PLAYERS 2
+#define MIN_STEPS   10
+
+enum BoardDirection {
+    clockwise = 0,
+    counterclockwise
+};
+
+class Game {
+   private:
+    int nPlayers;
+    int nSteps;
+    int nCoins;
+
+    DoublyLinkedList *gameBoard;
+
+   public:
+    Game();
+    ~Game();
+
+    void start();
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "doublyCircularLinkedList.h"
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	classDoublyCircularLinkedList list;
+	list.addElementBeginning(9);
+	list.print();
 	return 0;
 }
