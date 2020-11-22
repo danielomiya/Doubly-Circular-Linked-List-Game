84548dd446f2da369a2ddb749bfe39b3a53722da
Hugo <hugo.amartins.2019@gmail.com>
2020-11-22 18:00:35 -0300

    adding DCLL methods

--- doublyCircularLinkedList.cpp
+++ doublyCircularLinkedList.cpp
@@ -33,8 +33,8 @@ void classDoublyCircularLinkedList::addElementBeginning(int elementValue){
     numberOfElements++;
 }
 
-bool DoublyLinkedList::empty() {
-    return headElem == nullptr;
+bool classDoublyCircularLinkedList::empty() {
+    return tailElement == nullptr;
 }
 
 void classDoublyCircularLinkedList::print(){
@@ -51,9 +51,10 @@ void classDoublyCircularLinkedList::print(){
     
 }
 
-std::string DoublyLinkedList::stringify() {
+std::string classDoublyCircularLinkedList::stringify() {
+
     std::stringstream stream;
-    Element* cursor = headElem;
+    classElement * cursor = tailElement->getNextElementObject();
 
     if (cursor == nullptr) {
         return std::string("[ ]");
@@ -62,10 +63,89 @@ std::string DoublyLinkedList::stringify() {
     stream << "[ ";
 
     do {
-        stream << cursor->data() << " ";
-    } while (cursor != headElem);
+        stream << cursor->getElementValue() << " ";
+    } while (cursor != tailElement->getNextElementObject());
 
     stream << "]";
 
     return stream.str();
+}
+
+int classDoublyCircularLinkedList::getNumberOfElements(){
+    return numberOfElements;
+}
+
+void classDoublyCircularLinkedList::setNumberOfElements(int value){
+    numberOfElements = value;
+}
+
+void classDoublyCircularLinkedList::addElementEnd(int elementValue){
+    if (numberOfElements>0){
+        classElement * element;
+        element->setValue(elementValue);
+        classElement * headElement = tailElement->getNextElementObject();
+
+        tailElement->setNextElementObject(element);
+        headElement->setPreviousElementObject(element);
+        element->setNextElementObject(headElement);
+        element->setPreviousElementObject(tailElement);
+        tailElement = element;
+    }
+    else{
+        classElement * element;
+        element->setValue(elementValue);
+        tailElement = element;
+        element->setPreviousElementObject(element);
+        element->setNextElementObject(element);
+    }
+}
+
+void classDoublyCircularLinkedList::removeElement(int elementIndex){
+    classElement * cursor = tailElement;
+    cursor->getNextElementObject();
+    int count = 0;
+    
+    while (count != elementIndex){
+        cursor->getNextElementObject();
+        count++;
+    }
+
+    classElement * previousElement = cursor->getPreviousElementObject();
+    classElement * nextElement = cursor->getNextElementObject();
+
+    previousElement->setNextElementObject(nextElement);
+    nextElement->setPreviousElementObject(previousElement);
+}
+
+int classDoublyCircularLinkedList::elementValueByIndex(int index){
+    classElement * cursor = tailElement;
+    cursor->getNextElementObject();
+    int count = 0;
+    
+    while (count != index){
+        cursor->getNextElementObject();
+        count++;
+    }
+    
+    return cursor->getElementValue();
+}
+
+void classDoublyCircularLinkedList::addElement(int previousElement, int elementValue){
+    classElement * cursor = tailElement;
+    cursor->getNextElementObject();
+    int count = 0;
+    
+    while (count != previousElement){
+        cursor->getNextElementObject();
+        count++;
+    }
+
+    classElement * element;
+    element->setValue(elementValue);
+    classElement * nextElement;
+
+    element->setNextElementObject(nextElement);
+    element->setPreviousElementObject(cursor);
+    nextElement->setPreviousElementObject(element);
+    cursor->setNextElementObject(element);
 }
