d0b2f6a92fa25b124575310fd0f8590dddaae73f
Hugo <hugo.amartins.2019@gmail.com>
2020-11-22 21:21:49 -0300

    game loop and player initalization

--- game.cpp
+++ game.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sstream>
 #include "game.h"
+#include "utils.h"
 
 Game::Game() {
     gameBoard = new classDoublyCircularLinkedList();
@@ -9,6 +10,27 @@ Game::Game() {
 
 Game::~Game() {
     delete gameBoard;
+}
+
+int generateNextPrimeNumber(int past){
+    past++;
+    int flag = 0;
+
+    while (true){
+        int divisor = 0;
+        while (divisor!= past){
+            if (past%divisor == 0){
+                flag = 1;
+                break;
+            }
+            divisor ++;
+        }
+
+        if (flag == 0){
+            return past;
+        }
+        past++;
+    }
 }
 
 void Game::start() {
@@ -40,4 +62,71 @@ void Game::start() {
         nCoins = 15;
         std::cout << std::endl << "Quantidade de moedas deve ser maior que 0, assumindo 15" << std::endl << std::endl;
     }
+
+    int playersList[nPlayers];
+
+    int past = 1;
+    int prime = 0;
+
+    for (int i = 0; i < nPlayers; i++){
+        prime = generateNextPrimeNumber(past);
+        playersList[i] = generateNextPrimeNumber(prime);
+        past = prime;
+    }
+
+    for (int i = 0; i < nPlayers; i++){
+        int value = gameBoard->elementValueByIndex(0);
+        gameBoard->setElementValueByIndex(0, value + playersList[i]);
+    }
+
+    int stepsRegister[nSteps];
+
+    for (int i = 0; i < nSteps; i++){
+        stepsRegister[i] = 0;
+    }
+
+    while (true) {
+        /* gameloop */
+        for (int i = 0; i < nPlayers; i++){
+            int direction;
+            int diceQ = 0;
+            std::cout << "Digite 1 para escolher o sentído horário ou 0 para o anti-hoário\n";
+            std::cin >> direction;
+            std::cout << "Digite 1 para usar um dado ou 2 para dois dados\n";
+            std::cin >> diceQ;
+            int dice = randint(6 * diceQ);
+            std::cout << "Jogador " << i << "Você andará " << dice << " casas no sentido " << direction << "\n";
+            int cursor = 0;
+
+            while (gameBoard->elementValueByIndex(cursor) % playersList[i] != 0){
+                cursor++;
+            }
+
+            int elementValue = gameBoard->elementValueByIndex(cursor);
+            
+            gameBoard->setElementValueByIndex(cursor, elementValue - playersList[i]);
+
+            while (cursor != ((cursor + dice)%nSteps)){
+                cursor++;
+            }
+            
+            int elementValue = gameBoard->elementValueByIndex(cursor);
+            
+            gameBoard->setElementValueByIndex(cursor, elementValue + playersList[i]);
+
+            if (stepsRegister[cursor] != 0 && stepsRegister[cursor] != playersList[i]){
+                /* Ver a quem pertence e fazer as operações */
+            }
+            else{
+                if (stepsRegister[cursor] == playersList[i]){
+                    /* Colocar propriedade */
+                }
+                else{
+                    stepsRegister[cursor] = playersList[i];
+                }
+            }
+            
+        }
+    }
+    
 }
